8202ac3d35fc6518eaeaca36337f6371c9e22779
shoguntoto <85441789+shoguntoto@users.noreply.github.com>
2021-11-10 15:19:49 +0100

    Update help.js

--- Commande/help.js
+++ Commande/help.js
@@ -7,7 +7,8 @@ module.exports = {
         let help = new Discord.MessageEmbed()
         .setColor(`GREEN`)
         .setTitle(`Help de ${client.user.username}`)
-        .setDescription('**👋 Salut** ' + `${message.author},` + '\n**❗ Mon prefix est `=`**\n\n**:gear: ➜ Configuration**\n`verif-channel` : Ajouté le salon de vérification\n`salon-add` : Ajouté un salon publicitaire.\n`salon-remove` : Retiré un salon publicitaire.\n`salon-list` : Voir les salon publicitaire.\n\n**:robot: ➜ Bot**\n`bot-info` : Affiche les info du bot.\n`invitation` : Envoie le lien d\'invitation du bot.\n`support` : Envoie le serveur support du bot.\n`ping` : Affiche les ping du bot.\n\n[Invitation](https://discord.com/api/oauth2/authorize?client_id=848139058450137139&permissions=8&scope=bot) | [Support](https://discord.gg/DU75nykhBG)')
+        .setDescription('**👋 Salut** ' + `${message.author},` + '\n**❗ Mon prefix est `=`**\n\n**:gear: ➜ Configuration**\n`verif-channel` : Ajouté le salon de vérification\n`salon-add` : Ajouté un salon publicitaire.\n`salon-remove` : Retiré un salon publicitaire.\n`salon-list` : Voir les salon publicitaire.\n\n**:robot: ➜ Bot**\n`bot-info` : Affiche les info du bot.\n`invitation` : Envoie le lien d\'invitation du bot.\n`support` : Envoie le serveur support du bot.\n`ping` : Affiche les ping du bot.\n\n **⛔ ➜ Modération** \n`ban` : Bannir un membre \n`kick` : Expulser un membre \n`warn` : avertir un membre\n`checkwarn` : Voir le nombre de warn d un utilisateur\n`reset-warn` : enlever les warn d un utilisateur \n\n[Invitation](https://discord.com/api/oauth2/authorize?client_id=906455442698407956&permissions=8&scope=bot) | [Support](https://discord.gg/DU75nykhBG)')
+        .setFooter('By Shoguntoto (ショグントト)#1376 & ๖̶ζ͜͡AD MEGHOST')
 
     return message.channel.send(help)
     }}
