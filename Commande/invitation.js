1c2aaeaaa3b9a787a3b3f7c88e72cf64376df090
shoguntoto <85441789+shoguntoto@users.noreply.github.com>
2021-11-10 15:20:47 +0100

    Update invitation.js

--- Commande/invitation.js
+++ Commande/invitation.js
@@ -7,7 +7,7 @@ module.exports = {
         let help = new Discord.MessageEmbed()
         .setColor(`GREEN`)
         .setTitle(`Lien d'invitation de ${client.user.username}`)
-        .setDescription(`**Voici le lien d'invitation de ${client.user.username}**\n➜ [Cliquez ici](https://discord.com/api/oauth2/authorize?client_id=848139058450137139&permissions=8&scope=bot)`)
+        .setDescription(`**Voici le lien d'invitation de ${client.user.username}**\n➜ [Cliquez ici](https://discord.com/api/oauth2/authorize?client_id=906455442698407956&permissions=8&scope=bot)`)
 
     return message.channel.send(help)
     }}
