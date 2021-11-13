1329cacf80f68b7fc37782c4f668e1a7c8ea3fb8
meghost2004 <94074881+meghost2004@users.noreply.github.com>
2021-11-13 19:31:40 +0100

    Update and rename getpubchannels.js to salon-list.js

---
+++ Commande/salon-list.js
@@ -0,0 +1,23 @@
+const Discord = require('discord.js');
+const path = require('path');
+
+module.exports = {
+    name: 'salon-list',
+    run: async (message, args, client) => {
+    if(!args[0]){
+        let texte = '__Voici la liste des salons publicitaire du serveur.__\n';
+        const channels = require(path.resolve(path.join('./database/main.json')));
+        for(channel in channels[message.guild.id]){
+            texte = texte + `\n> <#${channel}>, **ID**: ${channel}`;
+        }
+        message.channel.send(
+            new Discord.MessageEmbed()
+                .setTitle('📃・Salons de publicité')
+                .setColor('GREEN')
+                .setDescription(texte + '\n\nPour en ajouter, faites ``q!salon-add`` et pour en enlever, ``q!salon-remove``.')
+                .setFooter(`${client.user.username}`)
+        )
+        
+    }
+    
+}}
