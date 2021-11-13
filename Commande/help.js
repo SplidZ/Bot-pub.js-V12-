0970790e29143a7b6bb1ba77f8cc1141e6f3938b
meghost2004 <94074881+meghost2004@users.noreply.github.com>
2021-11-13 20:06:58 +0100

    Update help.js

--- Commande/help.js
+++ Commande/help.js
@@ -7,8 +7,7 @@ module.exports = {
         let help = new Discord.MessageEmbed()
         .setColor(`GREEN`)
         .setTitle(`Help de ${client.user.username}`)
-        .setDescription('**👋 Salut** ' + `${message.author},` + '\n**❗ Mon prefix est `=`**\n\n**:gear: ➜ Configuration**\n`verif-channel` : Ajouté le salon de vérification\n`salon-add` : Ajouté un salon publicitaire.\n`salon-remove` : Retiré un salon publicitaire.\n`salon-list` : Voir les salon publicitaire.\n\n**:robot: ➜ Bot**\n`bot-info` : Affiche les info du bot.\n`invitation` : Envoie le lien d\'invitation du bot.\n`support` : Envoie le serveur support du bot.\n`ping` : Affiche les ping du bot.\n\n **⛔ ➜ Modération** \n`ban` : Bannir un membre \n`kick` : Expulser un membre \n`warn` : avertir un membre\n`checkwarn` : Voir le nombre de warn d un utilisateur\n`reset-warn` : enlever les warn d un utilisateur \n\n[Invitation](https://discord.com/api/oauth2/authorize?client_id=906455442698407956&permissions=8&scope=bot) | [Support](https://discord.gg/DU75nykhBG)')
-        .setFooter('By Shoguntoto (ショグントト)#1376 & ๖̶ζ͜͡AD MEGHOST')
-
+        .setDescription('**👋 Salut** ' + `${message.author},` + '\n**❗ Mon prefix est `q!`**\n\n**:gear: ➜ Configuration**\n`verif-channel` : Ajouté le salon de vérification\n`salon-add` : Ajouté un salon publicitaire.\n`salon-remove` : Retiré un salon publicitaire.\n`salon-list` : Voir les salon publicitaire.\n\n**🚨 ➜ Modération**\n`ban` : Bannir un membre.\n`kick` : Expulser un membre.\n`warn` : Avertir un membre.\n`checkwarn` : Voir le nombre de warn d\'un utilisateur.\n`reset-warn` : Enlever les warn d\'un utilisateur.\n\n**:robot: ➜ Bot**\n`bot-info` : Affiche les info du bot.\n`invitation` : Envoie le lien d\'invitation du bot.\n`support` : Envoie le serveur support du bot.\n`ping` : Affiche les ping du bot.\n\n[Invitation](https://discord.com/api/oauth2/authorize?client_id=906455442698407956&permissions=536870911991&scope=bot) | [Support](https://discord.gg/Xjf5mE4PPn)')
+.setFooter('By Shoguntoto (ショグントト)#1376 & ๖̶ζ͜͡AD MEGHOST')
     return message.channel.send(help)
     }}
