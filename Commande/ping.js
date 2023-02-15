434f6d2d95e84f944d0df4a7decd6b76790e3a1c
meghost2004 <94074881+meghost2004@users.noreply.github.com>
2023-02-15 09:58:59 +0100

    Delete Commande directory

--- Commande/ping.js
+++
@@ -1,9 +0,0 @@
-const Discord = require('discord.js');
-
-module.exports = {
-    name: 'ping',
-    run: async (message, args, client) => {
-        
-          message.channel.send(`🏓 ping du bot: **__${Math.round(client.ws.ping)}__** ms`);
-    },
-}
